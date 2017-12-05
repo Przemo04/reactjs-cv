256b65855409e4e220ca1f9046523fab5e2b0c36
Przemo04 <asus18@o2.pl>
2017-12-06 00:24:04 +0100

    add map skills from json

--- src/main/sectionSkills/SectionSkills.js
+++ src/main/sectionSkills/SectionSkills.js
@@ -3,43 +3,61 @@ import React from 'react';
 import './SectionSkills.scss';
 import HeadSection from '../../common/headSection/HeadSection.js';
 import Skill from './Skill/Skill.js';
+import skills from '../../data/skills.json';
+
+import ReactDOM from 'react-dom';
 class SectionSkills extends React.Component{
+
+	constructor(props) {
+		super(props);
+		this.state = { skills: []}
+	}
+
+	componentDidMount(){
+		this.setState({
+			skills
+		});
+	}
+
+	onClick(event){
+		event.preventDefault();
+		return false;
+	}
 	render(){
 
 		return(
 			<div className="SectionSkills">
 				<HeadSection name="Umiejętności" />
 				<h5>Legenda poziomu znajomości technologii i narzędzi*</h5>
-				<ul className="Legend-Skills">
-					<li>podstawowa</li>
-					<li><div className="Low"></div></li>
-					<li><div className="Basic"></div></li>
-					<li><div className="Medium"></div></li>
-					<li><div className="High"></div></li>
-					<li>zaawansowana</li>
-				</ul>
+					<ul className="Legend-Skills">
+						<li>podstawowa</li>
+						<li><div className="Low"><button onClick={this.onClick.bind(this)}>Click</button></div></li>
+						<li><div className="Basic"></div></li>
+						<li><div className="Medium"></div></li>
+						<li><div className="High"></div></li>
+						<li>zaawansowana</li>
+					</ul>
+
 				<div className="SkillWrapper">
-					<Skill level="Basic" name="JavaScript (ES6)"></Skill>
-					<Skill level="Low" name="Java"></Skill>
-					<Skill level="Basic" name="React"></Skill>
-					<Skill level="Low" name="Redux"></Skill>
-					<Skill level="Basic" name="GIT"></Skill>
-					<Skill level="Basic" name="SQL"></Skill>
-					<Skill level="High" name="Wordpress"></Skill>
-					<Skill level="Basic" name="SCRUM"></Skill>
-					<Skill level="Basic" name="jQuery"></Skill>
-					<Skill level="Low" name="PHP"></Skill>
-					<Skill level="Basic" name="Python/Django"></Skill>
-					<Skill level="High" name="SEO"></Skill>
-					<Skill level="High" name="HTML/CSS"></Skill>
-					<Skill level="Basic" name="Webpack"></Skill>
-					<Skill level="Basic" name="REST"></Skill>
-					<Skill level="Basic" name="Nginx"></Skill>
-					<Skill level="Medium" name="Linux"></Skill>
-					<Skill level="Basic" name="Jira"></Skill>
-					<Skill level="Medium" name="Bootstrap"></Skill>
-					<Skill level="Basic" name="FlexBox"></Skill>
+
+						{this.state.skills.map(item =>{
+							let classLevel;
+							if (item.level == 1) {
+								classLevel = 'Low';
+							} else if (item.level == 2){
+								classLevel = 'Basic';
+							} else if (item.level == 3){
+								classLevel = 'Medium';
+							} else {
+								classLevel = 'High';
+							}
+								return (
+									<Skill level={classLevel} name={item.name}></Skill>
+								);
+						})}
+
 				</div>
+
 				<p className="Star">* - Jest to subiektywna ocena umiejętności</p>
 
 			</div>
