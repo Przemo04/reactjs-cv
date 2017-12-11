1616d57bdcd51b2c1681bdfd2777e1427fe16740
Przemo04 <asus18@o2.pl>
2017-12-11 22:29:34 +0100

    add pretty syntax plugin, change filter skills

--- src/main/sectionSkills/SectionSkills.js
+++ src/main/sectionSkills/SectionSkills.js
@@ -1,97 +1,83 @@
-import React from 'react';
+import React from "react";
 
-import './SectionSkills.scss';
-import HeadSection from '../../common/headSection/HeadSection.js';
-import Skill from './Skill/Skill.js';
-import skills from '../../data/skills.json';
+import "./SectionSkills.scss";
+import HeadSection from "../../common/headSection/HeadSection.js";
+import Skill from "./Skill/Skill.js";
+import skills from "../../data/skills.json";
 
-import ReactDOM from 'react-dom';
-class SectionSkills extends React.Component{
+import ReactDOM from "react-dom";
+class SectionSkills extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { skills, level: "Low" };
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = { skills: []}
-	}
+  onClickChooseLevel(level, buttonId, event) {
+    event.preventDefault();
+    const currentLevel = this.state.level;
 
-	componentDidMount(){
-		this.setState({
-			skills
-		});
-	}
-	componentWillMount(buttonId){
-		this.setState({
-			skills
-		});
-	}
+    this.setState({
+      skills: skills,
+      level: level
+    });
+  }
 
-	onClickLow(buttonId, event){
-		event.preventDefault();
-		const filteredArray = this.state.skills.filter(item => item.level == buttonId);
-		this.setState({
-			skills: filteredArray
-		});
-	}
-	
-	onClickBasic(buttonId, event){
-		event.preventDefault();
-		const filteredArray = this.state.skills.filter(item => item.level == buttonId);
-		this.setState({
-			skills: filteredArray
-		});
-	}
-	onClickMedium(buttonId, event){
-		event.preventDefault();
-		const filteredArray = this.state.skills.filter(item => item.level == buttonId);
-		this.setState({
-			skills: filteredArray
-		});
-	}
-	onClickHigh(buttonId, event){
-		event.preventDefault();
-		const filteredArray = this.state.skills.filter(item => item.level == buttonId);
-		this.setState({
-			skills: filteredArray
-		});
-	}
-	render(){
+  render() {
+    return (
+      <div className="SectionSkills">
+        <HeadSection name="Umiejętności" />
+        <h5>Legenda poziomu znajomości technologii i narzędzi*</h5>
+        <ul className="Legend-Skills">
+          <li>podstawowa</li>
+          <li>
+            <div
+              onClick={this.onClickChooseLevel.bind(this, "Low", 1)}
+              className="Low"
+            />
+          </li>
+          <li>
+            <div
+              onClick={this.onClickChooseLevel.bind(this, "Basic", 2)}
+              className="Basic"
+            />
+          </li>
+          <li>
+            <div
+              onClick={this.onClickChooseLevel.bind(this, "Medium", 3)}
+              className="Medium"
+            />
+          </li>
+          <li>
+            <div
+              onClick={this.onClickChooseLevel.bind(this, "High", 4)}
+              className="High"
+            />
+          </li>
+          <li>zaawansowana</li>
+        </ul>
 
-		return(
-			<div className="SectionSkills">
-				<HeadSection name="Umiejętności" />
-				<h5>Legenda poziomu znajomości technologii i narzędzi*</h5>
-					<ul className="Legend-Skills">
-						<li>podstawowa</li>
-						<li><div onClick={this.onClickLow.bind(this, 1)} className="Low"></div></li>
-						<li><div onClick={this.onClickBasic.bind(this, 2)}className="Basic"></div></li>
-						<li><div onClick={this.onClickMedium.bind(this, 3)}className="Medium"></div></li>
-						<li><div onClick={this.onClickHigh.bind(this, 4)}className="High"></div></li>
-						<li>zaawansowana</li>
-					</ul>
-
-				<div className="SkillWrapper">
-
-						{this.state.skills.map(item =>{
-							let classLevel;
-							if (item.level == 1) {
-								classLevel = 'Low';
-							} else if (item.level == 2){
-								classLevel = 'Basic';
-							} else if (item.level == 3){
-								classLevel = 'Medium';
-							} else {
-								classLevel = 'High';
-							}
-								return (
-									<Skill level={classLevel} name={item.name}></Skill>
-								);
-						})}
-
-				</div>
-
-				<p className="Star">* - Jest to subiektywna ocena umiejętności</p>
-
-			</div>
-		);
-	}
+        <div className="SkillWrapper">
+          {this.state.skills.map(item => {
+            let classLevel;
+            if (item.level == 1) {
+              classLevel = "Low";
+            } else if (item.level == 2) {
+              classLevel = "Basic";
+            } else if (item.level == 3) {
+              classLevel = "Medium";
+            } else {
+              classLevel = "High";
+            }
+            if (classLevel == this.state.level) {
+              return (
+                <Skill key={item.id} level={classLevel} name={item.name} />
+              );
+            }
+          })}
+        </div>
+        <p className="Star">* - Jest to subiektywna ocena umiejętności</p>
+      </div>
+    );
+  }
 }
 export default SectionSkills;
--- src/main/sectionSkills/Skill/Skill.js
+++ src/main/sectionSkills/Skill/Skill.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './Skill.scss';
-class Skill extends React.Component{
-	render(){
-		return(
-			<div className="Skill">
-				<div className={this.props.level}>
-					<li>{this.props.name}
-					</li>
-				</div>
-			</div>
-		);
-	}
+import React from "react";
+import "./Skill.scss";
+class Skill extends React.Component {
+  render() {
+    return (
+      <div className="Skill">
+        <div className={this.props.level}>
+          <li>{this.props.name}</li>
+        </div>
+      </div>
+    );
+  }
 }
 export default Skill;
